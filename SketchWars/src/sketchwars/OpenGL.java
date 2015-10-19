60a83712ee4420c0621f9fc17a3b8851f3c5f674
Najash <najash.najm@gmail.com>
2015-10-19 10:42:53 -0700

    - Fullscreen bug where the window was not centered and scaled correctly when using system resolution that is not 1920x1080 is fixed

--- SketchWars/src/sketchwars/OpenGL.java
+++ SketchWars/src/sketchwars/OpenGL.java
@@ -107,14 +107,13 @@ public class OpenGL {
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glPushMatrix();
         GL11.glLoadIdentity();
+        GL11.glViewport(0,0,WIDTH,HEIGHT);
         GL11.glOrtho(0.0, WIDTH, 0.0, HEIGHT, -1.0, 1.0);
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
         GL11.glPushMatrix();
         GL11.glLoadIdentity();
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glEnable(GL11.GL_TEXTURE_2D);
-        
-        
     }
     
     public boolean windowsIsClosing() {
