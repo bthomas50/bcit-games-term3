f66e78e599950508e506d0d85c3e855100bb88a4
brian <bthomas50@my.bcit.ca>
2015-11-11 12:37:33 -0800

    fix compile err

--- SketchWars/src/packets/InputPacket.java
+++ SketchWars/src/packets/InputPacket.java
@@ -2,11 +2,12 @@ package packets;
 
 import sketchwars.input.Command;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputPacket extends GamePacket
 {
-	public List<Command> commands;
+	public ArrayList<Command> commands;
 
 	public InputPacket(byte id, byte frameId, List<Command> commands)
 	{
